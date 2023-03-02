00d35b0794f09292b1823ed0ee84c5c961e94c4e
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-02 16:57:52 +0300

    added header section

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import TaskList from './TaskList'
+
+
+function Header({tasks, onTaskDelete}) {
+    const renderTasksListAll = tasks.map(task => <TaskList task={task} key={task.id} onTaskDelete={onTaskDelete}/>)
+
+
+    return (
+        <div>
+            <h1 className='header'>Task List</h1>
+            <div>
+                <h2 className='all-todos-title'>All Tasks</h2>
+                {renderTasksListAll}
+                <br/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
